a760f6336e94d23bbfd1a0506c5bb5d29eedb3a0
Thijs Bouwes <thijsbouwes@gmail.com>
2018-05-28 21:17:39 +0200

    Change logo

--- resources/assets/js/layouts/external/Navigation.vue
+++ resources/assets/js/layouts/external/Navigation.vue
@@ -2,7 +2,7 @@
     <div class="navbar-fixed">
         <nav>
             <div class="nav-wrapper">
-                <a href="/" class="brand-logo center"><img src="/images/skihut_logo-48x48.png"> Skihut</a>
+                <a href="/" class="brand-logo center"><img src="/images/skihut_logo.png"> Skihut</a>
                 <ul class="right hide-on-med-and-down">
                     <router-link tag="li" to="/login" exact><a>Login</a></router-link>
                 </ul>
